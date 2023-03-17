0a31b1aa3bd5e8c5c75cf8ec1fff8f4a85e1b1b0
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-17 11:45:11 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'program that prints all the numbers of base 16 lower'
+ *
+ * Return: Always 0 (Success)
+ */
+int main() {
+    int i;
+    for (i = 0; i < 16; i++) {
+        if (i < 10) {
+            putchar('0' + i);
+        } else {
+            putchar('a' + i - 10);
+        }
+    }
+    putchar('\n');
+    return 0;
+}
