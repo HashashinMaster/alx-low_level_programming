d20af008692bb664e1cba5d7f2ff5e48b6a5b00f
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-17 11:49:44 -0700

    beeeeeeeeeeeeetttttty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,15 +10,14 @@
  *
  * Return: Always 0 (Success)
  */
-int main() {
+int main(void) {
 	int i;
 
     	for (i = 0; i < 16; i++)
         	if (i < 10)
             		putchar('0' + i);
 		else
-           		putchar('a' + i - 10);
-   
+           		putchar('a' + i - 10); 
     	putchar('\n');
     	return 0;
 }
