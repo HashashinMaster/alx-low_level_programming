ac0976b22d71714b4c0c9c90fce28eb863988323
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-17 11:18:04 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'Writing a program that prints all single digit numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+		printf("%d", i);
+	putchar('\n');
+	
+	return (0);
+}
