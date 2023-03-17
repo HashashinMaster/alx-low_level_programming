82cd85d6c8deed0a653d91576dc723edbe3d8885
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-17 11:29:49 -0700

    convertint to string

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'converting num to string'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
