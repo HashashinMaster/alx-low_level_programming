0bff10389ce34cd5516808825fd39458d3479f36
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-17 11:09:17 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Description: 'printing lowercharacters except q and e'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char lower[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; lower[i] != '\0'; i++)
+		if (lower[i] != 'q' && lower[i] != 'e') {
+			putchar(lower[i]);
+		}
+	putchar('\n');
+	return (0);
+}
