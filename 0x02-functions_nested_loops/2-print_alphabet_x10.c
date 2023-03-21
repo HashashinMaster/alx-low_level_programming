151f1e857b98c968ec72756c550cc48f63010779
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-21 12:13:03 -0700

    ah

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,7 +5,15 @@
 void print_alphabet_x10(void)
 {
 	int i;
+	int j;
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
 
 	for (i = 0; i < 9; i++)
-		print_alphabet();
+	{
+		for (j = 0; alphabets[i] != '\0'; i++)
+		{
+			_putchar(alphabets[i]);
+		}
+		_putchar('\n');
+	}
 }
