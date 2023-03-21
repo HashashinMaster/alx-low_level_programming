5fdc1199806bf6b9b2f4ddd55cfb241e95537c0e
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-21 15:11:40 -0700

    removing spaces

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,11 +1,13 @@
 #include "main.h"
 /**
-* jack_bauer - prints every minute of the day of Jack Bauer, from 00:00 to 23:59.
+* jack_bauer - prints every minute of the day of Jack Baurey.
+*
 * Return: Always 0.
 */
 void jack_bauer(void)
 {
 	int hour, minute;
+
 	for (hour = 0; hour < 24; hour++)
 	{
 		for (minute = 0; minute < 60; minute++)
