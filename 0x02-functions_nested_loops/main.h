ed2cec2cb5e63e0d83bd4248a52c96a9e41b8a15
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-21 11:53:53 -0700

    zbal kidayr

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -8,4 +8,14 @@
  * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c);
-void print_alphabet(void);
+void print_alphabet(void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; alphabets[i] != '\0'; i++)
+	{
+		_putchar(alphabets[i]);
+	}
+	_putchar('\n');
+}
