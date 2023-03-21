4bcbe60119f345be53e6735858e429879bcda7ba
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-21 10:44:36 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	char charString[] = "_putchar";
+
+    	for (i = 0; charString[i] != '\0'; i++)
+		_putchar(charString[i]);
+    	return (0);
+}
