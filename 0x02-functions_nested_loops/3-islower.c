8488efe67842aeac8499ce16aee37878db166e3e
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-21 12:41:50 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - checks if char is lower
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; alphabets[i] != '\0'; i++)
+	{
+		if (c == alphabets[i])
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
