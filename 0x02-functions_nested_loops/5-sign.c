d7aeee87b8facae1cd9d64b0ad01389906b8274a
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-21 13:11:43 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_sign - checks the sign_of_number
+ *
+ * @c: int
+ * Return: 0,1 or -1
+ *
+ */
+int print_sign(int c)
+{
+	int i;
+	
+	if (c > 1)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (c == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+
+}
