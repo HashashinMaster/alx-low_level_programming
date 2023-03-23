c032e638e5b348c9f5641bc7ccc62015eae33ea0
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-23 10:57:10 -0700

    idk what is the problem

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _isupper - check if parameter is upper
- * @c: The first integer
- *
- * Return: 0 or 1
- */
+* _isupper - check if parameter is upper
+* @c: The first integer
+*
+* Return: 0 or 1
+*/
 int _isupper(int c)
 {
 	char i;
