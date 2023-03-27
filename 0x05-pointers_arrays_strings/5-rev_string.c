a1a8d9aeecc405ed7fc42a10a59bc03d63319cb7
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-27 15:10:46 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,17 @@
+void rev_string(char *s)
+{
+	int n, i, j;
+	char c;
+
+	n = 0;
+	while (s[n] != '\0')
+		n++;
+
+	j = n - 1;
+	for (i = 0; j >= 0 && i < j; j--, i++)
+	{
+		c = s[i];
+		s[i] = s[j];
+		s[j] = c;
+	}
+}
