fd955e08f445485ab6389c2fae657ef0d80e99e7
WeedyDev <mehdibouzkoura1@gmail.com>
2023-03-30 10:47:49 -0700

    string to upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * string_toupper - change lowercase to uppercase
+ * @s:string
+ * Return:char
+ */
+char *string_toupper(char *p)
+{
+	for (; *p != '\0'; p++)
+		if ( *p >= 97 && *p <= 122)
+			*p -= 32;
+	return (p);
+}
