129b4956982a8e790c465e140b1d3460635a5a9c
WeedyDev <mehdibouzkoura1@gmail.com>
2023-04-03 15:29:15 -0700

    finall try

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#define NULL ((void *) 0)
 /**
  * _strchr - locates a character in a string
  * @s: string
@@ -16,5 +17,5 @@ char *_strchr(char *s, char c)
 			return (s);
 		s++;
 	}
-	return (0);
+	return (NULL);
 }
