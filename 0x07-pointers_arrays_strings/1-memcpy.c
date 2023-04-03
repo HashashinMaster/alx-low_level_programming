f95247d9527151511b3ce028f3f7155de3c99cea
WeedyDev <mehdibouzkoura1@gmail.com>
2023-04-03 14:33:56 -0700

    fixing betty style

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,9 +1,17 @@
+#include "main.h"
+
+/**
+ * _memcpy - function copies @n bytes from memory area @src
+ * @dest: pointer
+ * @src: pointer
+ * @n: positive int
+ * Return: dest pointer
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
 
 	for (; i < n; i++)
-		*(dest + i) = *(src +i);
-	
+		*(dest + i) = *(src + i);
 	return (dest);
 }
