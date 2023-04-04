63aec8be7b79b1322b1be2ee738d3f7193cde440
WeedyDev <mehdibouzkoura1@gmail.com>
2023-04-03 19:01:04 -0700

    just found u need stand library to use booleans lol

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,19 +1,30 @@
+#include "main.h"
+
+/**
+ * _strspn - a function that gets the length of a prefix substring
+ *
+ * @s: string
+ *
+ * @accept: string
+ *
+ * Return: int
+ */
 unsigned int _strspn(char *s, char *accept)
 {
-        unsigned int i, count = 0;
-	bool founded = false;
-        while (*s)
-        {
-                for (i = 0; *(accept + i) != '\0'; i++)
-                        if (*s == *(accept + i))
+	unsigned int i, count = 0, founded;
+
+	while (*s)
+	{
+		for (i = 0; *(accept + i) != '\0'; i++)
+			if (*s == *(accept + i))
 			{
 				count++;
-				founded = true;
+				founded = 1;
 			}
 		if (!founded)
 			break;
 		s++;
-		founded = false;
-        }
+		founded = 0;
+	}
 	return (count);
 }
