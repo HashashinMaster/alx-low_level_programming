f768caf5004c58f92ec5e6daeac857433cc89e7a
WeedyDev <mehdibouzkoura1@gmail.com>
2023-04-11 18:53:45 -0700

    please work

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,7 +11,7 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int s1Length = 0, s2Length = 0, length, s1NULL = 0, s2NULL = 0, i, j;
+	int s1Length = 0, s2Length = 0, length, i, j;
 	char *strConc;
 
 	if (s1 != NULL)
@@ -20,29 +20,18 @@ char *str_concat(char *s1, char *s2)
 			s1Length++;
 		s1Length++;
 	}
-	else
-		s1NULL = 1;
 	if (s2 != NULL)
-	{
 		while (*(s2 + s2Length) != '\0')
 			s2Length++;
-	}
-	else
-		s2NULL = 1;
-	length = s1Length + s2Length + s1NULL + s2NULL;
+	length = s1Length + s2Length;
 	strConc = malloc(length * sizeof(char));
-	if (s1 == NULL)
-		strConc[0] = ' ';
-	else
-		for (i = 0; s1[i] != '\0'; i++)
-			strConc[i] = s1[i];
-	if (s2 == NULL)
-		strConc[i] = ' ';
-	else
-		for (j = 0; s2[j] != '\0'; j++)
-			strConc[j + i] = s2[j];
+	if (strConc == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+		strConc[i] = s1[i];
+	for (j = 0; s2[j] != '\0'; j++)
+		strConc[j + i] = s2[j];
 	strConc[length] = '\0';
 	return (strConc);
 
-
-}
