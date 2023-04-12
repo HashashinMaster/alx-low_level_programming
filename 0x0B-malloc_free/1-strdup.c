afc4db59702daa3f3c1e92f069c8aa680497165a
WeedyDev <mehdibouzkoura1@gmail.com>
2023-04-11 18:07:57 -0700

    changing loop condition

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -20,7 +20,7 @@ char *_strdup(char *str)
 	strCpy = malloc(i * sizeof(*str));
 	if (strCpy == NULL)
 		return (NULL);
-	for (j = 0; j <= i; j++)
+	for (j = 0; j <= i + 1; j++)
 		*(strCpy + j) = *(str + j);
 	return (strCpy);
 }
